814c2b0778e5c4e66326f591ba80e997cce2a718
friska711 <126258596+friska711@users.noreply.github.com>
2023-06-13 19:57:14 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CounterSass from './components/counter-sass/counter'
+import Counter from './components/counter/counter'
+function App() {
+  return (
+    <div className="App">
+      <Counter/>
+      <CounterSass/>
+    </div>
+  );
+}
+
+export default App;
